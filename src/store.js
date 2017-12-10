86e4f084b06e3b8f05864a88a88e983f5ede62b1
zhang122622623 <1872591184@qq.com>
2017-12-10 19:43:05 +0800

    Eslintignore中加入src文件夹，reply组件化，增加vuex

---
+++ src/store.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+let state = {
+  topic: {},
+  replies: [],
+  replyTime:[]
+}
+
+const mutations = {
+  updateTopic(context,data){
+    context.topic = data
+  },
+  updateReplies(context,data){
+    context.replies = data
+  },
+  updateReplyTime(context,data){
+    context.replyTime = data
+  },
+}
+export default new Vuex.Store({
+  state,
+  mutations,
+})
